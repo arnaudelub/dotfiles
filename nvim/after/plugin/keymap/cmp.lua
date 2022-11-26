5e4d95af74b73b5d3360531b0d70f99fb17306c0
arnaud <arnaud@idelub.com>
2022-11-26 22:08:05 +0100

    added cmp for dartls and tsserver

--- nvim/after/plugin/keymap/cmp.lua
+++ nvim/after/plugin/keymap/cmp.lua
@@ -95,8 +95,14 @@ cmp.setup {
   },
 }
 -- Set up lspconfig.
-  local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-  require('lspconfig')['sumneko_lua'].setup {
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require('lspconfig')['sumneko_lua'].setup {
     capabilities = capabilities
-  }
+}
+require('lspconfig')['dartls'].setup {
+    capabilities = capabilities
+}
+require('lspconfig')['tsserver'].setup {
+    capabilities = capabilities
+}
