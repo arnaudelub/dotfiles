2c6ef6a3cb87233f7ec42fb9fad9f17fedf8f04d
arnauddelubacNexplore <arnaud.delubac@nexplore.com>
2025-01-17 22:38:20 +0100

    added nvim lua

---
+++ lua/nvim/lua/plugins/flutter.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    'nvim-flutter/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = function()
+      require('flutter-tools').setup {
+        decorations = {
+          statusline = {
+            -- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
+            -- this will show the current version of the flutter app from the pubspec.yaml file
+            app_version = true,
+            -- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
+            -- this will show the currently running device if an application was started with a specific
+            -- device
+            device = true,
+            -- set to true to be able use the 'flutter_tools_decorations.project_config' in your statusline
+            -- this will show the currently selected project configuration
+            project_config = true,
+          },
+        },
+        debugger = {
+          enabled = true,
+        },
+      }
+    end,
+  },
+}
